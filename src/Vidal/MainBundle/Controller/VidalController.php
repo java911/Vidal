65763b887553c571f21c0578912d36d3e6890076
Artem <7binary@gmail.com>
2013-12-06 17:18:25 +0400

    testing swiftmailer

--- src/Vidal/MainBundle/Controller/VidalController.php
+++ src/Vidal/MainBundle/Controller/VidalController.php
@@ -473,14 +473,15 @@ class VidalController extends Controller
 	 */
 	public function testEmail()
 	{
-		$message = \Swift_Message::newInstance()
-			->setSubject('Сообщение из формы обратной связи')
-			->setContentType('text/html')
-			->setCharset('utf-8')
-			->setFrom('noreply@mailbot.evrika.ru', 'Testing')
-			->setTo('7binary@gmail.com')
-			->setBody('<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>Its a simple message</body></html>');
+//		$message = \Swift_Message::newInstance()
+//			->setSubject('Сообщение из формы обратной связи')
+//			->setContentType('text/html')
+//			->setCharset('utf-8')
+//			->setFrom('noreply@mailbot.evrika.ru', 'Testing')
+//			->setTo('7binary@gmail.com')
+//			->setBody('<!DOCTYPE html><html><head><meta charset="utf-8"></head><body>Its a simple message</body></html>');
 
-		$this->get('mailer')->send($message);
+		mail('7binary@bk.ru','oyee','it is works');
+		//$this->get('mailer')->send($message);
 	}
 }
