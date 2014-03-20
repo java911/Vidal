51d70737cd9349df1b9964a6dd1e960ea87f9d3f
Maxim <bhd.m@ya.ru>
2014-03-20 16:11:22 +0400

    update symptoms page

--- src/Vidal/MainBundle/Controller/SymptomController.php
+++ src/Vidal/MainBundle/Controller/SymptomController.php
@@ -4,7 +4,9 @@ namespace Vidal\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Response;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Vidal\MainBundle\Entity\DiseaseParty;
 
 class SymptomController extends Controller{
 
@@ -17,9 +19,26 @@ class SymptomController extends Controller{
     }
 
     /**
-     * @Route("/symptom/{type}/{id}", name="symptom_ajax")
-     * @Template()
+     * @Route("/symptom/{party}", name="render_symptom", defaults = { "party"="all" }, options={"expose"=true})
+     * @Template("VidalMainBundle:Disease:symptomList.html.twig")
      */
-    public function ajaxSymptomAction($type,$id){}
+    public function SymptomListAction($party = 'all'){
+        $party = $this->getDoctrine()->getRepository('VidalMainBundle:DiseaseParty')->findOneById($party);
+        $symptoms = $party->getSymptoms();
+        return array(
+            'symptoms'   => $symptoms,
+        );
+    }
+
+    /**
+     * @Route("/symptom-body/{sex}/{party}", name="symptom_body", defaults = { "sex"="1" , "party"="all" }, options={"expose"=true})
+     * @Template("VidalMainBundle:Disease:body.html.twig")
+     */
+    public function bodyAction($sex = 1, $party = 'all'){
+        return array(
+            'sex'   => $sex,
+            'party'   => $party,
+        );
+    }
 
 }
