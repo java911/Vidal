5b2047ac47710491d09dda97f0833b88f27cb801
Maxim <bhd.m@ya.ru>
2014-03-24 17:59:55 +0400

    add service market

---
+++ src/Vidal/MainBundle/Controller/MarketController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Vidal\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Vidal\MainBundle\Entity\DiseaseParty;
+
+class MarketController extends Controller{
+    protected $basket;
+
+
+}
