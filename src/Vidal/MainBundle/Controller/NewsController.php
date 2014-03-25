6978ac9b2dfc6eeb445297ff69c067abfedd6d81
Artem <7binary@gmail.com>
2014-03-25 14:19:58 +0400

    updates

---
+++ src/Vidal/MainBundle/Controller/NewsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Vidal\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class NewsController extends Controller
+{
+}
---
+++ src/Vidal/MainBundle/Tests/Controller/NewsControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Vidal\MainBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class NewsControllerTest extends WebTestCase
+{
+}
