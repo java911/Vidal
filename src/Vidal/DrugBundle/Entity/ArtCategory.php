eb25633dfc3b50cfc014fc0e684037709f6c6891
Artem <7binary@gmail.com>
2014-04-14 16:14:23 +0400

    reworked art @ admin

---
+++ src/Vidal/DrugBundle/Entity/ArtCategory.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Vidal\DrugBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/** @ORM\Entity @ORM\Table(name="art_type") */
+class ArtType
+{
+	/** @ORM\Column(type="integer") @ORM\Id @ORM\GeneratedValue */
+	protected $id;
+
+	/** @ORM\Column(length=255, unique=true) */
+	protected $title;
+
+	/** @ORM\OneToMany(targetEntity="Art", mappedBy="type") */
+	protected $arts;
+
+	/** @ORM\ManyToOne(targetEntity="ArtRubrique", inversedBy="types") */
+	protected $rubrique;
+
+	public function __construct()
+	{
+		$this->arts = new ArrayCollection();
+	}
+
+	public function __toString()
+	{
+		return empty($this->title) ? '' : $this->title;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getId()
+	{
+		return $this->id;
+	}
+
+	/**
+	 * @param mixed $title
+	 */
+	public function setTitle($title)
+	{
+		$this->title = $title;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getTitle()
+	{
+		return $this->title;
+	}
+
+	/**
+	 * @param mixed $arts
+	 */
+	public function setArts($arts)
+	{
+		$this->arts = $arts;
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getArts()
+	{
+		return $this->arts;
+	}
+}
