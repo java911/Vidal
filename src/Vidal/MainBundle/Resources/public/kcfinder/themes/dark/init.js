bed8421bce8659d453e12d7539b1a38bb65fb38f
Artem <7binary@gmail.com>
2014-04-23 12:46:11 +0400

    downgraded kcfinder to 2.51

--- src/Vidal/MainBundle/Resources/public/kcfinder/themes/dark/init.js
+++ src/Vidal/MainBundle/Resources/public/kcfinder/themes/dark/init.js
@@ -1,1 +1,4 @@
-new Image().src = 'themes/dark/img/loading.gif'; // preload animated gif
+// If this file exists in theme directory, it will be loaded in <head> section
+
+var imgLoading = new Image();
+imgLoading.src = 'themes/dark/img/loading.gif';
