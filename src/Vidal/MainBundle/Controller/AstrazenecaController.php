a777afb425cdb6ca623165a4f9f13c1739d8eca0
bhd.m <bhd.m@ya.ru>
2014-05-22 14:06:55 +0400

    update map

--- src/Vidal/MainBundle/Controller/AstrazenecaController.php
+++ src/Vidal/MainBundle/Controller/AstrazenecaController.php
@@ -72,7 +72,11 @@ class AstrazenecaController extends Controller
      * @Template("VidalMainBundle:Astrazeneca:map_xml.html.twig")
      */
     public function mapXmlAction(){
-        $coords = $this->getDoctrine()->getRepository('VidalMainBundle:AstrazenecaMap')->findAll();
+        $coords[0] = $this->getRequest()->query->get('x1');
+        $coords[1] = $this->getRequest()->query->get('y1');
+        $coords[2] = $this->getRequest()->query->get('x2');
+        $coords[3] = $this->getRequest()->query->get('y2');
+        $coords = $this->getDoctrine()->getRepository('VidalMainBundle:AstrazenecaMap')->findCoords($coords);
         return array('coords' => $coords);
     }
 
