494c651ac38393af3be8a1cfd8528e428cc3d843
Artem <7binary@gmail.com>
2014-05-26 18:24:28 +0400

    vixed /vracham arts

--- src/Vidal/DrugBundle/Command/ArtLinkCommand.php
+++ src/Vidal/DrugBundle/Command/ArtLinkCommand.php
@@ -7,34 +7,62 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class ArticleCommand extends ContainerAwareCommand
+class ArtLinkCommand extends ContainerAwareCommand
 {
 	protected function configure()
 	{
-		$this->setName('vidal:article');
+		$this->setName('vidal:art_link');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
 	{
 		ini_set('memory_limit', -1);
-		$output->writeln('--- vidal:article started');
+		$output->writeln('--- vidal:art_link started');
 
 		$em = $this->getContainer()->get('doctrine')->getManager('drug');
 
-		$articles = $em->createQuery("
+		$arts = $em->createQuery("
 			SELECT a
-			FROM VidalDrugBundle:Article a
-			WHERE a.body LIKE '%href=\"http://www.vidal.ru/%'
+			FROM VidalDrugBundle:Art a
+			WHERE a.link IS NULL
+			 	OR a.link = ''
 		")->getResult();
 
-		foreach ($articles as $article) {
-			$body = $article->getBody();
-			$body = preg_replace('#href="http://www\.vidal\.ru/#', 'href="/', $body);
-			$article->setBody($body);
+		foreach ($arts as $art) {
+			$title = $art->getTitle();
+			$art->setLink($this->generateLink($title));
 		}
 
 		$em->flush();
 
-		$output->writeln("+++ vidal:article completed!");
+		$output->writeln("+++ vidal:art_link completed!");
+	}
+
+	protected function generateLink($title)
+	{
+		$rus = array(
+			'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й',
+			'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф',
+			'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я',
+			'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й',
+			'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф',
+			'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я',
+			'—', '-', '  ', ',', '.', '!', '?', '/',
+		);
+
+		$eng = array(
+			'A', 'B', 'V', 'G', 'D', 'E', 'IO', 'ZH', 'Z', 'I', 'Y',
+			'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F',
+			'H', 'TS', 'CH', 'SH', 'SCH', '', 'Y', '', 'E', 'YU', 'IA',
+			'a', 'b', 'v', 'g', 'd', 'e', 'io', 'zh', 'z', 'i', 'y',
+			'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f',
+			'h', 'ts', 'ch', 'sh', 'sch', '', 'y', '', 'e', 'yu', 'ia',
+			'_', '_', '_', '_', '_', '_', '_', '_',
+		);
+
+		$title = str_replace($rus, $eng, $title);
+		$title = mb_strtolower($title, 'utf-8');
+
+		return $title;
 	}
 }
--- src/Vidal/MainBundle/Controller/ArticleController.php
+++ src/Vidal/MainBundle/Controller/ArticleController.php
@@ -333,30 +333,22 @@ class ArticleController extends Controller
 		$count = count($parts);
 
 		if ($count == 1) {
-			if ($rubrique->getTypes()->count() == 0) {
-				$params['pagination'] = $this->get('knp_paginator')->paginate(
-					$em->getRepository('VidalDrugBundle:Art')->getQueryByRubrique($rubrique),
-					$request->query->get('p', 1),
-					self::ARTICLES_PER_PAGE
-				);
-			}
-			else {
-				$params['types'] = $em->getRepository('VidalDrugBundle:ArtType')->findByRubrique($rubrique);
-			}
+			$params['types']      = $em->getRepository('VidalDrugBundle:ArtType')->findByRubrique($rubrique);
+			$params['pagination'] = $this->get('knp_paginator')->paginate(
+				$em->getRepository('VidalDrugBundle:Art')->getQueryByRubrique($rubrique),
+				$request->query->get('p', 1),
+				self::ARTICLES_PER_PAGE
+			);
 		}
 		elseif ($count == 2) {
-			$type           = $em->getRepository('VidalDrugBundle:ArtType')->rubriqueUrl($rubrique, $parts[1]);
-			$params['type'] = $type;
-			if ($params['type']->getCategories()->count() == 0) {
-				$params['pagination'] = $this->get('knp_paginator')->paginate(
-					$em->getRepository('VidalDrugBundle:Art')->getQueryByType($type),
-					$request->query->get('p', 1),
-					self::ARTICLES_PER_PAGE
-				);
-			}
-			else {
-				$params['categories'] = $em->getRepository('VidalDrugBundle:ArtCategory')->findByType($type);
-			}
+			$type                 = $em->getRepository('VidalDrugBundle:ArtType')->rubriqueUrl($rubrique, $parts[1]);
+			$params['type']       = $type;
+			$params['categories'] = $em->getRepository('VidalDrugBundle:ArtCategory')->findByType($type);
+			$params['pagination'] = $this->get('knp_paginator')->paginate(
+				$em->getRepository('VidalDrugBundle:Art')->getQueryByType($type),
+				$request->query->get('p', 1),
+				self::ARTICLES_PER_PAGE
+			);
 		}
 		elseif ($count == 3) {
 			$type                 = $em->getRepository('VidalDrugBundle:ArtType')->rubriqueUrl($rubrique, $parts[1]);
