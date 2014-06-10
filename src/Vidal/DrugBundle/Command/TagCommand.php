12bce9a3af47eb0ca2e53c83898bfba6f0991c2b
Artem <7binary@gmail.com>
2014-06-10 17:54:33 +0400

    added TagCommand for PharmArticle

--- src/Vidal/DrugBundle/Command/TagCommand.php
+++ src/Vidal/DrugBundle/Command/TagCommand.php
@@ -6,17 +6,13 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Vidal\DrugBundle\Entity\Tag;
 
-class TestCommand extends ContainerAwareCommand
+class TagCommand extends ContainerAwareCommand
 {
 	protected function configure()
 	{
-		$this
-			->setName('vidal:_')
-			->setDescription('Command to copy')
-			//->addArgument('user', InputArgument::OPTIONAL, '')
-			//->addOption('email', null, InputOption::VALUE_NONE, '')
-		;
+		$this->setName('vidal:tag');
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
@@ -24,8 +20,34 @@ class TestCommand extends ContainerAwareCommand
 		ini_set('memory_limit', -1);
 		$em = $this->getContainer()->get('doctrine')->getManager('drug');
 
-		$output->writeln('--- vidal: started');
+		$output->writeln('--- vidal:tag started');
 
-		$output->writeln('+++ vidal: completed');
+		$articles = $em->createQuery('
+			SELECT a
+			FROM VidalDrugBundle:PharmArticle a
+		')->getResult();
+
+		foreach ($articles as $article) {
+			if ($company = $article->getCompany()) {
+				$text = $company->getTitle();
+				$tag = $em->createQuery('
+					SELECT t
+					FROM VidalDrugBundle:Tag t
+					WHERE t.text = :text
+				')->setParameter('text', $text)
+					->getOneOrNullResult();
+
+				if (!$tag) {
+					$tag = new Tag();
+					$tag->setText($text);
+					$em->persist($tag);
+				}
+
+				$article->addTag($tag);
+				$em->flush();
+			}
+		}
+
+		$output->writeln('+++ vidal:tag completed');
 	}
 }
