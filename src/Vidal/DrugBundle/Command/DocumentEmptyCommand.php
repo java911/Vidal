55e3e3c2d6eb8c14b85e49d32b0040ac5696199b
Artem <7binary@gmail.com>
2014-07-07 12:22:31 +0400

    added command to clean empty fields of documents @ DocumentEmptyCommand

---
+++ src/Vidal/DrugBundle/Command/DocumentEmptyCommand.php
@@ -0,0 +1,43 @@
+<?php
+namespace Vidal\DrugBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class DocumentEmptyCommand extends ContainerAwareCommand
+{
+	protected function configure()
+	{
+		$this->setName('vidal:document_empty');
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		ini_set('memory_limit', -1);
+		$output->writeln('--- vidal:document_empty started');
+
+		$em = $this->getContainer()->get('doctrine')->getManager('drug');
+		$pdo = $em->getConnection();
+		$fields = explode(' ', 'CompaniesDescription ClPhGrDescription ClPhGrName PhInfluence PhKinetics Dosage OverDosage Interaction Lactation SideEffects StorageCondition Indication ContraIndication SpecialInstruction RenalInsuf HepatoInsuf PharmDelivery ElderlyInsuf ChildInsuf');
+
+		foreach ($fields as $field) {
+			$this->cleanField($field, $pdo, $output);
+		}
+
+		$output->writeln("... DELETE FROM document WHERE YearEdition > 2014 OR (CountryEditionCode != 'RUS' AND ArticleID NOT IN (1,4))");
+		$stmt = $pdo->prepare("DELETE FROM document WHERE YearEdition > 2014 OR (CountryEditionCode != 'RUS' AND ArticleID NOT IN (1,4))");
+		$stmt->execute();
+
+		$output->writeln("+++ vidal:document_empty completed!");
+	}
+
+	private function cleanField($field, $pdo, $output)
+	{
+		$output->writeln("... cleaning $field");
+		$stmt = $pdo->prepare("UPDATE document SET $field = NULL WHERE $field IN ('<P><P/>', '<p></p>');");
+		$stmt->execute();
+	}
+}
