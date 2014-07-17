0cb727cc78bd2bdf303a28eb931586f03bd8886a
Artem <7binary@gmail.com>
2014-07-17 11:41:08 +0400

    added tag command for articles/publications

---
+++ src/Vidal/DrugBundle/Entity/TagRepository.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Vidal\DrugBundle\Entity;
+
+use Doctrine\ORM\EntityRepository;
+
+class PublicationRepository extends EntityRepository
+{
+	public function findLast($top = 5)
+	{
+		return $this->_em->createQuery('
+			SELECT p
+			FROM VidalDrugBundle:Publication p
+			WHERE p.enabled = TRUE
+				AND p.date < :now
+				AND p.priority IS NULL
+			ORDER BY p.date DESC
+		')->setParameter('now', new \DateTime())
+			->setMaxResults($top)
+			->getResult();
+	}
+
+	public function findLastPriority($top = 3)
+	{
+		return $this->_em->createQuery('
+			SELECT p
+			FROM VidalDrugBundle:Publication p
+			WHERE p.enabled = TRUE
+				AND p.date < :now
+				AND p.priority IS NOT NULL
+			ORDER BY p.priority DESC, p.date DESC
+		')->setParameter('now', new \DateTime())
+			->setMaxResults($top)
+			->getResult();
+	}
+
+	public function findFrom($from, $max)
+	{
+		return $this->_em->createQuery('
+			SELECT p
+			FROM VidalDrugBundle:Publication p
+			WHERE p.enabled = TRUE
+				AND p.date < :now
+			ORDER BY p.priority DESC, p.date DESC
+		')->setParameter('now', new \DateTime())
+			->setFirstResult($from)
+			->setMaxResults($max)
+			->getResult();
+	}
+
+	public function getQueryEnabled()
+	{
+		return $this->_em->createQuery('
+			SELECT p
+			FROM VidalDrugBundle:Publication p
+			WHERE p.enabled = TRUE
+				AND p.date < :now
+				AND p.priority IS NULL
+			ORDER BY p.date DESC
+		')->setParameter('now', new \DateTime());
+	}
+
+	public function getQueryByTag($tagId)
+	{
+		return $this->_em->createQuery('
+			SELECT p
+			FROM VidalDrugBundle:Publication p
+			JOIN p.tags t
+			WHERE p.enabled = TRUE
+				AND p.date < :now
+				AND t = :tagId
+			ORDER BY p.priority DESC, p.date DESC
+		')->setParameter('now', new \DateTime())
+			->setParameter('tagId', $tagId);
+	}
+}
