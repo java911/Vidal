81be893da8a6a22517e76fdd8422c864365885e7
Artem <7binary@gmail.com>
2014-07-25 18:02:35 +0400

    added command to rebuild tags relations

--- src/Vidal/DrugBundle/Command/TagCommand.php
+++ src/Vidal/DrugBundle/Command/TagCommand.php
@@ -20,27 +20,154 @@ class TagCommand extends ContainerAwareCommand
 		ini_set('memory_limit', -1);
 		$em = $this->getContainer()->get('doctrine')->getManager('drug');
 		$output->writeln('--- vidal:tag started');
+		$pdo  = $em->getConnection();
+		$date = '2014-05-14 00:00:00';
 
-		$companies = file(__DIR__ . DIRECTORY_SEPARATOR . 'doc.txt');
-		$pdo       = $em->getConnection();
+		# новости
+		$publications = $em->createQuery("
+					SELECT p.id
+					FROM VidalDrugBundle:Publication p
+					WHERE p.date < '$date'
+				")->getResult();
 
-		foreach ($companies as $company) {
-			$company = trim($company);
-			$company = $this->mb_ucfirst($company);
+		foreach ($publications as $o) {
+			$id = $o['id'];
+			$pdo->prepare("DELETE FROM publication_tag WHERE publication_id = $id")->execute();
+		}
 
-			$stmt = $pdo->prepare("UPDATE tag SET text = '$company' WHERE text LIKE '$company'");
-			$stmt->execute();
+		# статьи энц
+		$articles = $em->createQuery("
+					SELECT a.id
+					FROM VidalDrugBundle:Article a
+					WHERE a.date < '$date'
+				")->getResult();
+
+		foreach ($articles as $o) {
+			$id = $o['id'];
+			$pdo->prepare("DELETE FROM article_tag WHERE article_id = $id")->execute();
+		}
+
+		# статьи спец
+		$arts = $em->createQuery("
+					SELECT a.id
+					FROM VidalDrugBundle:Art a
+					WHERE a.date < '$date'
+				")->getResult();
+
+		foreach ($arts as $o) {
+			$id = $o['id'];
+			$pdo->prepare("DELETE FROM art_tag WHERE art_id = $id")->execute();
+		}
+
+		# статьи фарм-компаний
+		$pharmArticles = $em->createQuery("
+					SELECT a.id
+					FROM VidalDrugBundle:PharmArticle a
+					WHERE a.created < '$date'
+				")->getResult();
+
+		foreach ($pharmArticles as $o) {
+			$id = $o['id'];
+			$pdo->prepare("DELETE FROM pharmarticle_tag WHERE pharmarticle_id = $id")->execute();
+		}
+
+		############################################################################################
+		############################################################################################
+		############################################################################################
+		# проставляем все теги
+
+		$tags = $em->createQuery('
+			SELECT t.text, t.id
+			FROM VidalDrugBundle:Tag t
+		')->getResult();
+
+		foreach ($tags as $tag) {
+			$text  = trim($tag['text']);
+			$tagId = $tag['id'];
+
+			# проставляем тег у статей энкициклопедии
+			$articles = $em->createQuery('
+					SELECT a.id
+					FROM VidalDrugBundle:Article a
+					WHERE (a.title LIKE :text1 OR a.title LIKE :text2 OR a.title LIKE :text3 OR a.title LIKE :text4 OR a.title LIKE :text5)
+						OR (a.announce LIKE :text1 OR a.announce LIKE :text2 OR a.announce LIKE :text3 OR a.announce LIKE :text4 OR a.announce LIKE :text5)
+						OR (a.body LIKE :text1 OR a.body LIKE :text2 OR a.body LIKE :text3 OR a.body LIKE :text4 OR a.body LIKE :text5)
+				')->setParameters(array(
+					'text1' => '% ' . $text . '%',
+					'text2' => '%"' . $text . '%',
+					'text3' => '%,' . $text . '%',
+					'text4' => '%.' . $text . '%',
+					'text5' => '%(' . $text . '%',
+				))->getResult();
+
+			foreach ($articles as $a) {
+				$id   = $a['id'];
+				$stmt = $pdo->prepare("INSERT IGNORE INTO article_tag (tag_id, article_id) VALUES ($tagId, $id)");
+				$stmt->execute();
+			}
+
+			# проставляем тег у статей специалистам
+			$arts = $em->createQuery('
+					SELECT a.id
+					FROM VidalDrugBundle:Art a
+					WHERE (a.title LIKE :text1 OR a.title LIKE :text2 OR a.title LIKE :text3 OR a.title LIKE :text4 OR a.title LIKE :text5)
+						OR (a.announce LIKE :text1 OR a.announce LIKE :text2 OR a.announce LIKE :text3 OR a.announce LIKE :text4 OR a.announce LIKE :text5)
+						OR (a.body LIKE :text1 OR a.body LIKE :text2 OR a.body LIKE :text3 OR a.body LIKE :text4 OR a.body LIKE :text5)
+				')->setParameters(array(
+					'text1' => '% ' . $text . '%',
+					'text2' => '%"' . $text . '%',
+					'text3' => '%,' . $text . '%',
+					'text4' => '%.' . $text . '%',
+					'text5' => '%(' . $text . '%',
+				))->getResult();
+
+			foreach ($arts as $a) {
+				$id   = $a['id'];
+				$stmt = $pdo->prepare("INSERT IGNORE INTO art_tag (tag_id, art_id) VALUES ($tagId, $id)");
+				$stmt->execute();
+			}
+
+			# проставляем тег у новостей
+			$publications = $em->createQuery('
+					SELECT a.id
+					FROM VidalDrugBundle:Publication a
+					WHERE (a.title LIKE :text1 OR a.title LIKE :text2 OR a.title LIKE :text3 OR a.title LIKE :text4 OR a.title LIKE :text5)
+						OR (a.announce LIKE :text1 OR a.announce LIKE :text2 OR a.announce LIKE :text3 OR a.announce LIKE :text4 OR a.announce LIKE :text5)
+						OR (a.body LIKE :text1 OR a.body LIKE :text2 OR a.body LIKE :text3 OR a.body LIKE :text4 OR a.body LIKE :text5)
+				')->setParameters(array(
+					'text1' => '% ' . $text . '%',
+					'text2' => '%"' . $text . '%',
+					'text3' => '%,' . $text . '%',
+					'text4' => '%.' . $text . '%',
+					'text5' => '%(' . $text . '%',
+				))->getResult();
+
+			foreach ($publications as $p) {
+				$id   = $p['id'];
+				$stmt = $pdo->prepare("INSERT IGNORE INTO publication_tag (tag_id, publication_id) VALUES ($tagId, $id)");
+				$stmt->execute();
+			}
+
+			# проставляем тег у новостей фарм-компаний
+			$pharmArticles = $em->createQuery('
+					SELECT a.id
+					FROM VidalDrugBundle:PharmArticle a
+					WHERE a.text LIKE :text1 OR a.text LIKE :text2 OR a.text LIKE :text3 OR a.text LIKE :text4 OR a.text LIKE :text5
+				')->setParameters(array(
+					'text1' => '% ' . $text . '%',
+					'text2' => '%"' . $text . '%',
+					'text3' => '%,' . $text . '%',
+					'text4' => '%.' . $text . '%',
+					'text5' => '%(' . $text . '%',
+				))->getResult();
+
+			foreach ($pharmArticles as $p) {
+				$id   = $p['id'];
+				$stmt = $pdo->prepare("INSERT IGNORE INTO pharmarticle_tag (tag_id, pharmarticle_id) VALUES ($tagId, $id)");
+				$stmt->execute();
+			}
 		}
 
 		$output->writeln('+++ vidal:tag completed');
-	}
-
-	private function mb_ucfirst($string, $encoding = 'utf-8')
-	{
-		$strlen    = mb_strlen($string, $encoding);
-		$firstChar = mb_substr($string, 0, 1, $encoding);
-		$then      = mb_substr($string, 1, $strlen - 1, $encoding);
-
-		return mb_strtoupper($firstChar, $encoding) . $then;
 	}
 }
