0815acdb8d32f1790fd252caef4f28617557bf58
bhdm <bhd.m@ya.ru>
2014-08-25 15:13:00 +0400

    update counter

--- src/Vidal/MainBundle/Controller/IndexController.php
+++ src/Vidal/MainBundle/Controller/IndexController.php
@@ -459,14 +459,27 @@ class IndexController extends Controller
      */
     public function firstSetAction(){
         $em = $this->getDoctrine()->getManager();
-        $user = $em->getRepository('VidalMainBundle:User')->find($this->getUser()->getId());
-        if ($user==null || $user->getFirstset() == false){
-            $firstset = false;
-        }else{
-            $user->setFirstset(true);
-            $em->flush($user);
-            $firstset = true;
+        $firstset = false;
+        if ($this->getUser()){
+            $user = $em->getRepository('VidalMainBundle:User')->find($this->getUser()->getId());
+            if ($user==null || $user->getFirstset() == false){
+                $firstset = false;
+            }else{
+                $user->setFirstset(true);
+                $em->flush($user);
+                $firstset = true;
+            }
         }
         return array('firstset' => $firstset);
+    }
+
+    /**
+     * @Route("/profile/{userId}", name="profile_user")
+     * @Template()
+     */
+    public function profileAction($userId){
+        $user = $this->getDoctrine()->getRepository('VidalMainBundle:User')->find($userId);
+
+        return array('user' => $user);
     }
 }
