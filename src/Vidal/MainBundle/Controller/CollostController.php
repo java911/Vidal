bf0322ca930142c4c4a4cfd5d6492d5aa8d37010
bhdm <bhd.m@ya.ru>
2014-10-15 16:05:50 +0400

    add collost

---
+++ src/Vidal/MainBundle/Controller/CollostController.php
@@ -0,0 +1,21 @@
+<?php
+namespace Vidal\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+
+class CollostController extends Controller
+{
+    /**
+     * @Route("/collost" ,name="collost")
+     * @template()
+     */
+    public function indexAction(Request $request){
+        return array();
+    }
+}
