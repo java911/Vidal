417de78fa1624dd4baf837d782f7535df5a1d9d3
7binary <7binary@gmail.com>
2015-02-27 14:29:52 +0300

    updates @ delivery

--- src/Vidal/MainBundle/Command/DigestCommand.php
+++ src/Vidal/MainBundle/Command/DigestCommand.php
@@ -17,7 +17,9 @@ class DigestCommand extends ContainerAwareCommand
 			->addOption('stop', null, InputOption::VALUE_NONE, 'Stop sending digests')
 			->addOption('clean', null, InputOption::VALUE_NONE, 'Clean log app/logs/digest_sent.txt')
 			->addOption('all', null, InputOption::VALUE_NONE, 'Send digest to every subscribed user')
-			->addOption('me', null, InputOption::VALUE_NONE, 'Send digest to 7binary@gmail.com');;
+			->addOption('me', null, InputOption::VALUE_NONE, 'Send digest to 7binary@gmail.com')
+			->addOption('local', null, InputOption::VALUE_NONE, 'Send digest from 7binary@list.ru')
+		;
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output)
@@ -26,13 +28,14 @@ class DigestCommand extends ContainerAwareCommand
 		set_time_limit(0);
 
 		# опции не указаны - выводим мануал
-		if (!$input->getOption('test') && !$input->getOption('clean') && !$input->getOption('all') && !$input->getOption('me')) {
+		if (!$input->getOption('test') && !$input->getOption('clean') && !$input->getOption('all') && !$input->getOption('me') && !$input->getOption('me')) {
 			$output->writeln('=> Error: uncorrect syntax. READ BELOW');
 			$output->writeln('$ php app/console evrika:digest --test');
 			$output->writeln('$ php app/console evrika:digest --stop');
 			$output->writeln('$ php app/console evrika:digest --clean');
 			$output->writeln('$ php app/console evrika:digest --all');
 			$output->writeln('$ php app/console evrika:digest --me');
+			$output->writeln('$ php app/console evrika:digest --me --local');
 			return false;
 		}
 
@@ -54,14 +57,16 @@ class DigestCommand extends ContainerAwareCommand
 			return false;
 		}
 
+		# рассылка всем подписанным врачам
 		if ($input->getOption('all')) {
-			# рассылка всем подписанным врачам
+
 			$output->writeln("Sending: in progress to ALL subscribed users...");
 			$digest->setProgress(true);
 			$this->sendToAll();
 		}
-		elseif ($input->getOption('test')) {
-			# рассылка нашим менеджерам
+
+		# рассылка нашим менеджерам
+		if ($input->getOption('test')) {
 			$raw      = explode(';', $digest->getEmails());
 			$emails[] = array();
 
@@ -72,11 +77,15 @@ class DigestCommand extends ContainerAwareCommand
 			$output->writeln("Sending: in progress to managers: " . implode(', ', $emails));
 			$this->sendTo($emails);
 		}
-		elseif ($input->getOption('me')) {
+
+		# отправить самому себе
+		if ($input->getOption('me')) {
 			$output->writeln("Sending: in progress to 7binary@gmail.com");
-			$this->sendTo(array('7binary@gmail.com'));
+			$this->sendTo(array('7binary@gmail.com'), $input->getOption('local'));
 		}
-		elseif ($input->getOption('clean')) {
+
+
+		if ($input->getOption('clean')) {
 			$output->writeln('Cleaned sent flag of users!');
 		}
 
@@ -88,7 +97,7 @@ class DigestCommand extends ContainerAwareCommand
 	 *
 	 * @param array $emails
 	 */
-	private function sendTo(array $emails)
+	private function sendTo(array $emails, $local = false)
 	{
 		$container  = $this->getContainer();
 		$em         = $container->get('doctrine')->getManager();
@@ -109,12 +118,11 @@ class DigestCommand extends ContainerAwareCommand
 			$template2 = $templating->render('VidalMainBundle:Digest:template2.html.twig', array('user' => $user));
 			$template = $template1 . $template2;
 
-			$test = true;
-			$this->send($user['username'], $user['firstName'], $template, $subject, $test);
+			$this->send($user['username'], $user['firstName'], $template, $subject, $local);
 		}
 	}
 
-	public function send($email, $to, $body, $subject, $test = false)
+	public function send($email, $to, $body, $subject, $local = false)
 	{
 		$mail = new \PHPMailer();
 
@@ -129,7 +137,7 @@ class DigestCommand extends ContainerAwareCommand
 		$mail->addAddress($email, $to);
 		$mail->addCustomHeader('Precedence', 'bulk');
 
-		if ($test) {
+		if ($local) {
 			$mail->Host       = 'smtp.gmail.com';
 			$mail->From       = 'binacy@gmail.com';
 			$mail->SMTPSecure = 'ssl';
